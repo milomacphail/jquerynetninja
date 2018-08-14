7bc5a5f0197b9036918db42dd8af6f71282f5d54
Milo MacPhail <milomacphail@gmail.com>
2018-08-14 23:19:16 +0000

    Lesson-5

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-$(document).ready(function() {
-    alert("jquery ready!");
-});
+$("h3").css({border: "3px solid blue"});
+
+$(".wrapper").css({border:"3px solid red"});
+
+$("#clients").css({border:"3px solid yellow"});
+    
