456402e408c6cae7b363720181d827a7b84e96d7
Milo MacPhail <milomacphail@gmail.com>
2018-08-15 14:33:22 +0000

    Lesson 8

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-/*$("#contact-methods").next().css({border:"2px solid pink"});*/
-
-/*$("#social-nav").prev().css({border:"2px solid blue"});*/
-
-/*$(".banner-title").parent().css({border:"3px solid pink"});*/
-
-/*$(".banner-title").parents().css({border:"3px solid pink"});*/
-
-/*$("#social-nav").children().css({border:"3px solid green"});*/
-
-/*$("#contact").find(".facebook").css({border:"3px solid purple"});*/
-
-/*$("#social-nav").closest(".wrapper").css({border:"3px solid orange"});*/
+$("#contact-methods").css({border: "2px solid red"})
+.next().css({border: "2px solid green"})
+.closest("section").css({border:"2px solid blue"});
 
 
 
