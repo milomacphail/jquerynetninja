e0706c7bd008915a221a9ff9396e84d0d94705df
Milo MacPhail <milomacphail@gmail.com>
2018-08-15 14:02:24 +0000

    Lesson 6

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-$("h3").css({border: "3px solid blue"});
+/*$("header nav li:first").css({border: "2px solid red"});
 
-$(".wrapper").css({border:"3px solid red"});
+$("header nav li:last").css({border: "2px solid red"});*/
 
-$("#clients").css({border:"3px solid yellow"});
-    
+/*$("#contact ul:first").css({border: "2px solid red"});
+$("#contact ul:last").css({"border: 2px solid red"});*/
+
+/*$("header nav li:even").css({border: 2px solid blue});
+$("header nav li:odd").css({border: 2px solid yellow});*/
+
+/*$("section:not('#contact')").css({border: "2px solid green"});*/
+
+/*$("#social-nav li:lt(3)").css({border: "2px solid blue"});*/
+/*$("#social-nav li:gt(2)").css({border: "2px solid blue"});*/
+
+/*$("img[alt]").css({border: "2px solid pink"});*/
+/*$("img[alt=quote]").css({border:"2px solid purple"});*/
+
+
+
