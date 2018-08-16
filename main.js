5391f94596d16079b0326c0ac417b9ebe7e57ffa
Milo MacPhail <milomacphail@gmail.com>
2018-08-16 02:03:51 +0000

    Lesson 14

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-$("#social-nav").css({
-    ":top": "400px",
-    "left": "150px",
-    "opacity":"0.5",
-    "border-top": "4px solid red"
-});
+var button = $("#lead-banner a");
+
+button[0].onclick = function(){
+    $("points-of-sale").toggleClass("open");
+    return false;
+};
