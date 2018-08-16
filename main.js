47af5e044e5071d9a5798948586894eadc141e60
Milo MacPhail <milomacphail@gmail.com>
2018-08-16 23:23:50 +0000

    Lesson 23

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
     
-   $(".slide-button-up").on("click", function(){
-     
-     $("#lead-banner").slideToggle(1000,  function(){
-        alert("Animation complete!")
-     });
-   });
-    
+    var allQuotes = $("blockquote");
+    var currentQuote= 0;
+    function changeQuote(){
+       
+       $(allQuotes[currentQuote]).fadeOut(200, function(){
+          if (currentQuote == allQuotes.length-1){
+          currentQuote =[0];
+       } else {
+          currentQuote++;
+       }
+       $(allQuotes[currentQuote]).fadeIn(200);
+       });
+       
+    }
+    var quoteTimer = setInterval(changeQuote, 3000);
 });
