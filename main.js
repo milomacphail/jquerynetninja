36f55f83c1a96a3e40196227016e3c5ae39c4430
Milo MacPhail <milomacphail@gmail.com>
2018-08-16 23:31:53 +0000

    Lesson-24

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
 $(document).ready(function(){
     
-    var allQuotes = $("blockquote");
-    var currentQuote= 0;
-    function changeQuote(){
-       
-       $(allQuotes[currentQuote]).fadeOut(200, function(){
-          if (currentQuote == allQuotes.length-1){
-          currentQuote =[0];
-       } else {
-          currentQuote++;
-       }
-       $(allQuotes[currentQuote]).fadeIn(200);
-       });
-       
-    }
-    var quoteTimer = setInterval(changeQuote, 3000);
+    var items = $("#points-of-sale li");
+    items.on("click", function(){
+       $(this).find("p").slideToggle(500);
+    });
+    
 });
