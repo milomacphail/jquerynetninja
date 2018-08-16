4c54a38008b851c6fcb0860e5ac79298bcbbefa9
Milo MacPhail <milomacphail@gmail.com>
2018-08-16 23:04:58 +0000

    Lesson 22

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     
-   $("img[alt=map]").on("click", function(){
-      
-      $("section > h2").toggle(1000);
-      
+   $(".slide-button-up").on("click", function(){
+     
+     $("#lead-banner").slideToggle(1000,  function(){
+        alert("Animation complete!")
+     });
    });
     
 });
